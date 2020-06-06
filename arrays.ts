8a875e9a02cdf9fc312030d212dd41075d38deb9
asadikhlas <asadikhlas03@gmail.com>
2020-06-06 23:50:13 +0500

    feat: ts array

--- arrays.ts
+++ arrays.ts
@@ -1,5 +1,17 @@
-const carMakers: string[] = ['Ford','toyota','Chevy']
+const carMakers: string[] = ['Ford', 'toyota', 'Chevy'];
 
-const cardMakeBy: string[][] = [['abc'], ['fa'], ['fas']]
+const cardMakeBy: string[][] = [['abc'], ['fa'], ['fas']];
 
-const newArray: (string | number)[] = [1, 2, "Asad"]
+const newArray: (string | number)[] = [1, 2, 'Asad'];
+
+//Help with inference when extracting values
+const car: string  = carMakers[0]
+const mycar: string = carMakers.pop()
+
+//Prevent incompatible values
+carMakers.push(100);
+
+//Help with array helpers
+carMakers.map((car: string): string => {
+  return car.toUpperCase();
+});
