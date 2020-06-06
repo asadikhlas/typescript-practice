0ec7ee3e805423a2137e58a347071a713ce9b59a
asadikhlas <asadikhlas03@gmail.com>
2020-06-06 22:41:18 +0500

    feat: function annotation

---
+++ features/functions.ts
@@ -0,0 +1,15 @@
+const add = (a: number, b: number): number => {
+    return a + b
+}
+
+const sub = (a: number, b: number): number => {
+    return a - b
+}
+
+function devide (a: number, b: number): number{
+    return a / b
+}
+
+const multiply = (a: number, b: number):number => {
+    return a * b
+}
