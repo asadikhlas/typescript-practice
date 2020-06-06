49e202f3901bfa24994fb4ec3229015ff2d2fc99
asadikhlas <asadikhlas03@gmail.com>
2020-06-06 20:33:00 +0500

    feat: practice with network request and types in ts

--- features/variables.ts
+++ features/variables.ts
@@ -6,3 +6,17 @@ let nothingMuch: null = null
 let nothing: undefined = undefined
 
 let now: Date = new Date()
+
+//Array
+
+let colors: string[] = ["red","green","blue"]
+let num: number[] = [1,2,3];
+let truth: boolean[] = [true, false, true]
+
+//class
+
+class Car {
+
+}
+
+let car: Car = new Car()
