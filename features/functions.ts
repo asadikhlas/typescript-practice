82e0b618e2ee10f7196ce51c0eb880fc7336f68c
asadikhlas <asadikhlas03@gmail.com>
2020-06-06 23:21:18 +0500

    feat: add destructuring

--- features/functions.ts
+++ features/functions.ts
@@ -13,3 +13,17 @@ function devide (a: number, b: number): number{
 const multiply = (a: number, b: number):number => {
     return a * b
 }
+
+//Destructuring
+
+const todaysWeather: {date: Date, weather: string} = {
+    date: new Date(),
+    weather: 'Sunny'
+}
+
+const logWeather = ({date, weather}: {date: Date, weather: string}) => {
+    console.log(date)
+    console.log(weather)
+}
+
+logWeather(todaysWeather)
