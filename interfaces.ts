6904ce0100482d93e20ac567609405332c35154a
asadikhlas <asadikhlas03@gmail.com>
2020-06-07 20:13:26 +0500

    feat: interferaces

--- interfaces.ts
+++ interfaces.ts
@@ -1,19 +1,21 @@
 interface Vehicle {
   name: string;
-  year: number;
+  year: Date;
   broken: boolean;
+  summary(): string;
 }
 
 const oldCivic = {
   name: 'civic',
-  year: 2000,
+  year: new Date(),
   broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  },
 };
 
 const printVehicle = (vehicle: Vehicle): void => {
-  console.log(`vehicle name: ${vehicle.name}`);
-  console.log(`vehicle year: ${vehicle.year}`);
-  console.log(`broken? ${vehicle.broken}`);
+  console.log(vehicle.summary());
 };
 
 printVehicle(oldCivic);
