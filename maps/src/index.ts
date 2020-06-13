a048dbfa4d4f332b7216822265974ddf7f4b7712
asadikhlas <asadikhlas03@gmail.com>
2020-06-14 00:28:46 +0500

    added instance for custom maps

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,13 +1,7 @@
 // import { User } from './User'
 // import { Company } from './Company'
+import { CustomMap } from "./CustomMap";
 
+new CustomMap("map");
 // const user = new User()
 // console.log(user)
-
-new google.maps.Map(document.getElementById("map"), {
-  zoom: 2,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
