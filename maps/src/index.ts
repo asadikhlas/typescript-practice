dd93928deaf217ae3286f1a055f4aa0ac22121d8
asadikhlas <asadikhlas03@gmail.com>
2020-06-13 23:05:46 +0500

    integrating google map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,4 +1,13 @@
-import { User } from './User'
+// import { User } from './User'
+// import { Company } from './Company'
 
-const user = new User()
-console.log(user)
+// const user = new User()
+// console.log(user)
+
+new google.maps.Map(document.getElementById("map"), {
+  zoom: 2,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
